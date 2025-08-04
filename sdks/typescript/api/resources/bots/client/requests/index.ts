cfef993c628fd95a071f551225413348633e47f5
AvaterClasher <116944847+AvaterClasher@users.noreply.github.com>
2025-08-04 19:43:15 +0530

    feat(bots): add TypeScript SDK components for bot management, including client methods for listing, creating, updating, retrieving, and deleting bots, along with request types for API interactions

---
+++ sdks/typescript/api/resources/bots/client/requests/index.ts
@@ -0,0 +1,2 @@
+export { type GetV1BotsRequest } from "./GetV1BotsRequest.js";
+export { type UpdateBotRequest } from "./UpdateBotRequest.js";
---
+++ sdks/typescript/api/resources/index.ts
@@ -0,0 +1,7 @@
+export * as calls from "./calls/index.js";
+export * from "./calls/types/index.js";
+export * as bots from "./bots/index.js";
+export * as phoneNumbers from "./phoneNumbers/index.js";
+export * from "./bots/client/requests/index.js";
+export * from "./calls/client/requests/index.js";
+export * from "./phoneNumbers/client/requests/index.js";
