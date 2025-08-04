13be1c202b32361beeecf58ba7a5556f6e34c4e0
AvaterClasher <116944847+AvaterClasher@users.noreply.github.com>
2025-08-04 19:42:25 +0530

    feat(errors): add auto-generated error classes for BadRequestError, ConflictError, InternalServerError, NotFoundError, and UnauthorizedError

---
+++ sdks/typescript/api/errors/index.ts
@@ -0,0 +1,5 @@
+export * from "./BadRequestError.js";
+export * from "./UnauthorizedError.js";
+export * from "./InternalServerError.js";
+export * from "./NotFoundError.js";
+export * from "./ConflictError.js";
