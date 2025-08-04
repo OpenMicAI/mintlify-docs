3ffe13855db0680a722b01fb2ff302665201ba15
AvaterClasher <116944847+AvaterClasher@users.noreply.github.com>
2025-08-04 19:41:43 +0530

    feat(types): add TypeScript SDK components including OpenmicApiClient and environment configuration for API interaction

---
+++ sdks/typescript/index.ts
@@ -0,0 +1,4 @@
+export * as OpenmicApi from "./api/index.js";
+export { OpenmicApiError, OpenmicApiTimeoutError } from "./errors/index.js";
+export { OpenmicApiClient } from "./Client.js";
+export { OpenmicApiEnvironment } from "./environments.js";
