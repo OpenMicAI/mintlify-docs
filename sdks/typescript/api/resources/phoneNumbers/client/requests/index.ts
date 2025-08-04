9e17f4324a1ab7a2b1010b52b7e42703110c8e35
AvaterClasher <116944847+AvaterClasher@users.noreply.github.com>
2025-08-04 19:45:39 +0530

    feat(phone_numbers): add TypeScript request interfaces for managing phone numbers, including Get, Link, and Unlink requests

---
+++ sdks/typescript/api/resources/phoneNumbers/client/requests/index.ts
@@ -0,0 +1,3 @@
+export { type GetV1PhoneNumbersRequest } from "./GetV1PhoneNumbersRequest.js";
+export { type LinkBotToPhoneNumberRequest } from "./LinkBotToPhoneNumberRequest.js";
+export { type PostV1PhoneNumbersUnlinkAgentRequest } from "./PostV1PhoneNumbersUnlinkAgentRequest.js";
